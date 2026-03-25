402e059b64edc6d55dfe91a5f634e14a941958b8
Khan Jun Ming, Amos <101790228+amoskhan@users.noreply.github.com>
2026-03-25 21:44:31 +0800

    Set dev server port to 5173
    Update vite.config.ts to change the development server port from 3000 to 5173.

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   return {
     clearScreen: true,
     server: {
-      port: 3000,
+      port: 5173,
       host: '0.0.0.0',
       proxy: {
         '/bedrock-api': {
